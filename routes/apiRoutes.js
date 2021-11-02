b7d676ebe2d6501b0891911f27aaeff1176ef11a
DBueno1464 <DerrickBueno00@gmail.com>
2021-11-02 01:05:15 -0400

    cleaned up routes

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -23,17 +23,6 @@ router.post("/api/workouts", (req, res) => {
     });
 });
 
-// combined weight
-// router.get("/api/workouts", (req, res) => {
-//   Workout.aggregate({})
-//     .then((data) => {
-//       res.json(data);
-//     })
-//     .catch((err) => {
-//       res.status(400).json(err);
-//     });
-// });
-
 // add exercise
 router.put("/api/workouts/:id", (req, res) => {
   Workout.findByIdAndUpdate(req.params.id, {$push: { exercises: req.body}},
