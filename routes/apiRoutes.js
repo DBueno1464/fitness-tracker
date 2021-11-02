0fe61a2219bb6db77892892f5cbd0b73e27d9249
DBueno1464 <DerrickBueno00@gmail.com>
2021-11-02 01:03:29 -0400

    add exercise routes created

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -12,7 +12,7 @@ router.get("/api/workouts", (req, res) => {
     });
 });
 
-// create a workout plan
+// create a workout
 router.post("/api/workouts", (req, res) => {
   Workout.create({})
     .then((data) => {
@@ -21,6 +21,33 @@ router.post("/api/workouts", (req, res) => {
     .catch((err) => {
       res.status(400).json(err);
     });
+});
+
+// combined weight
+// router.get("/api/workouts", (req, res) => {
+//   Workout.aggregate({})
+//     .then((data) => {
+//       res.json(data);
+//     })
+//     .catch((err) => {
+//       res.status(400).json(err);
+//     });
+// });
+
+// add exercise
+router.put("/api/workouts/:id", (req, res) => {
+  Workout.findByIdAndUpdate(req.params.id, {$push: { exercises: req.body}},
+    {
+      new: true,
+      runValidators: true
+    }
+  )
+  .then(data => {
+    res.json(data);
+  })
+  .catch(err => {
+    res.status(400).json(err);
+  })
 });
 
 module.exports = router;
--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -4,21 +4,15 @@ const { Workout } = require("../models");
 
 
 router.get('/', (req, res) => {
-
-    res.sendFile(path.join(__dirname, "../public/index.html"));
-    
+    res.sendFile(path.join(__dirname, "../public/index.html"));  
 });
 
 router.get('/stats', (req, res) => {
-
-    res.sendFile(path.join(__dirname, "../public/stats.html"));
-    
+    res.sendFile(path.join(__dirname, "../public/stats.html")); 
 });
 
 router.get('/exercise', (req, res) => {
-
-    res.sendFile(path.join(__dirname, "../public/exercise.html"));
-    
+    res.sendFile(path.join(__dirname, "../public/exercise.html")); 
 });
 
 module.exports = router;
