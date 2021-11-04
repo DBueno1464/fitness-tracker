eff51e23cb43ff89ce65a44c57ddbd4bd33621b1
DBueno1464 <DerrickBueno00@gmail.com>
2021-11-04 18:52:36 -0400

    added .get in range

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -51,4 +51,25 @@ router.put("/api/workouts/:id", (req, res) => {
     });
 });
 
+// get workouts in range
+router.get("/api/workouts/range", (req, res) => {
+  Workout.aggregate([
+    {
+      $addFields: {
+        totalDuration: {
+          $sum: "$exercises.duration",
+        },
+      },
+    },
+  ])
+    .sort({ _id: -1 })
+    .limit(7)
+    .then((data) => {
+      res.json(data);
+    })
+    .catch((err) => {
+      res.status(400).json(err);
+    });
+});
+
 module.exports = router;
